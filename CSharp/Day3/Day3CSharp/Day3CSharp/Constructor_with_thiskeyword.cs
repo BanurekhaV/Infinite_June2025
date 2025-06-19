a4803bd5e1a0f1cd2be83c92f0f972d1e1bf0277
BanurekhaV <rekhasairam@hotmail.com>
2025-06-19 18:39:33 +0530

    Day3csgharp

---
+++ CSharp/Day3/Day3CSharp/Day3CSharp/Constructor_with_thiskeyword.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace Day3CSharp
+{
+
+    class Customer
+    {
+        public Customer()
+        {
+            Console.WriteLine("Customer constructor 1...");
+        }
+
+        public Customer(int a) : this()
+        {
+            Console.WriteLine("Customer constructor 2...");
+        }
+
+        public Customer(string s, int x):this(50)
+        {
+            Console.WriteLine("Customer constructor 3...");
+        }
+}
+    class Constructor_with_thiskeyword
+    {
+        static void Main()
+        {
+            Customer cust = new Customer("Hello", 10);
+            Console.Read();
+        }
+    }
+}
---
+++ CSharp/Day3/Day3CSharp/Day3CSharp/ProtectedConstructors.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace Day3CSharp
+{
+    class Dog
+    {
+        public string Name;
+        public int Age;
+
+        protected Dog()
+        {
+            Console.WriteLine("We are in the process of constructing a Dog..");
+            Console.WriteLine(Name + " "+ Age);
+        }
+    }
+
+    class Labdrador : Dog
+    {
+        public double measurements;
+
+        public Labdrador(string name, int age, double m)
+        {
+            Console.WriteLine("Labrador getting constructed..");
+            Name = name;
+            Age = age;
+            measurements = m;
+            Console.WriteLine(name + " " + age + " " + measurements);
+        }
+    }
+
+    class ProtectedConstructors
+    {
+        static void Main()
+        {
+            Labdrador l = new Labdrador("Tuffy", 5, 12.5);
+            Console.Read();
+        }
+    }
+}
