73710c359453335938efdbdad18722d297c87f0b
BanurekhaV <rekhasairam@hotmail.com>
2025-06-19 11:47:31 +0530

    Day3 CSharp

---
+++ CSharp/Day3/Day3CSharp/Day3CSharp/MethodsnParameters.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace Day3CSharp
+{
+    class MethodsnParameters
+    {
+        //by value
+
+        public static void SimpleValueMethod(int j)
+        {
+            j = 100;
+            Console.WriteLine("J's value is " +  j);
+        }
+
+        //by reference
+        public static void SimpleRefMethod(ref int j)
+        {
+            j = 100;
+            Console.WriteLine("J's value is " + j);
+        }
+    }
+
+    class Tester
+    {
+        static void Main()
+        {
+            int i = 10;
+            MethodsnParameters.SimpleValueMethod(i);
+            Console.WriteLine("I's values is {0}", i);
+            Console.WriteLine("__________________");
+            MethodsnParameters.SimpleRefMethod(ref i);
+            Console.WriteLine("I's values is {0}", i);
+            Console.Read();
+        }
+    }
+}
---
+++ CSharp/Day3/Day3CSharp/Day3CSharp/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Day3CSharp
+{
+    class Employee
+    {
+        int EmpId;
+        string EmpName;
+        DateTime DOJ;
+        double Salary;
+
+        //constructor 1
+         internal Employee()
+        {
+            EmpId = 1;
+            EmpName = "Adithya";
+            DOJ = Convert.ToDateTime("01/06/2025");
+            Salary = 35000;
+        }
+
+        //constructor 2
+        public Employee(int empid, string ename)
+        {
+            EmpId = empid;
+            EmpName = ename;
+        }
+        //constructor 3
+        public Employee(int EmpId, string EmpName, DateTime DOJ, double Salary)
+        {
+            this.EmpId = EmpId;
+            this.EmpName = EmpName;
+            this.DOJ = DOJ;
+            this.Salary = Salary;
+        }
+
+        //destructor
+        ~ Employee()
+        {
+            Console.WriteLine("Bye from Employee..");
+            Console.Read();
+        }
+        public void ShowEmployee()
+        {
+            Console.WriteLine($"{EmpId}, {EmpName}, {DOJ}, and {Salary}");
+        
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.ShowEmployee();
+            Employee employee2 = new Employee(100, "Akhilesh");
+            employee2.ShowEmployee();
+            Employee employee3 = new Employee(101, "Dinesh", Convert.ToDateTime("06/02/2025"), 35000);
+            employee3.ShowEmployee();                
+            Console.Read();
+        }
+
+    }
+}
