24dec9ac02e815a9c474e3d948cb89a3247fd266
BanurekhaV <rekhasairam@hotmail.com>
2025-06-24 18:44:42 +0530

    indexers

--- CSharp/Day5/Day5Csharp/Day5Csharp/Employees.cs
+++ CSharp/Day5/Day5Csharp/Day5Csharp/Employees.cs
@@ -12,7 +12,7 @@ namespace Day5Csharp
         public string Gender { get; set; }
         public string Department { get; set; }
 
-        //declaring indexers for the class
+        //declaring indexers for the class with integer subscript
         public object this[int index]
         {
             get
@@ -48,6 +48,44 @@ namespace Day5Csharp
                     Department = value.ToString();
             }
         }
+
+        //declaring indexers for the class with string subscript
+        //overloading an indexer with the property names
+        public object this[string index]
+        {
+            get
+            {
+                if (index.ToLower() == "id")
+                    return Id;
+                else if (index.ToLower() =="ename")
+                    return EName;
+                else if (index.ToLower() == "job")
+                    return Job;
+                else if (index.ToLower() == "salary")
+                    return Salary;
+                else if (index.ToLower() == "gender")
+                    return Gender;
+               else if (index.ToLower() == "department")
+                    return Department;
+                else
+                    return null;
+            }
+            set
+            {
+                if (index =="Id")
+                    Id = Convert.ToInt32(value);
+                else if (index == "EName")
+                    EName = value.ToString();
+                else if (index == "Job")
+                    Job = value.ToString();
+                else if (index.ToLower() == "salary")
+                    Salary = Convert.ToDouble(value);
+                else if (index == "Gender")
+                    Gender = value.ToString();
+                else if (index == "Department")
+                    Department = value.ToString();
+            }
+        }
     }
 
     class IndexersEg
@@ -68,6 +106,31 @@ namespace Day5Csharp
             Console.WriteLine("Employee Name = " + emp[1]);
             Console.WriteLine("Employee Salary = " + emp[3]);
             Console.WriteLine("Employee Works For = " + emp[5] + " Department");
+
+            emp[1] = "Jayavardhan";
+            emp[3] = 36000;
+
+            Console.WriteLine("=========After modifications========");
+            Console.WriteLine("Employee Id =" + emp[0]);
+            Console.WriteLine("Employee Name = " + emp[1]);
+            Console.WriteLine("Employee Salary = " + emp[3]);
+            Console.WriteLine("Employee Works For = " + emp[5] + " Department");
+
+            Console.WriteLine("=======Accessing indexer with propertyname overload =====");
+
+            Console.WriteLine("Employee Id =" + emp["Id"]);
+            Console.WriteLine("Employee Name = " + emp["EName"]);
+            Console.WriteLine("Employee Salary = " + emp["Salary"]);
+            Console.WriteLine("Employee Works For = " + emp["Department"] + " Department");
+
+            emp["EName"] = "Nithin Kumar";
+            emp["salary"] = 36500;
+
+            Console.WriteLine("=========After modifications========");
+            Console.WriteLine("Employee Id =" + emp["id"]);
+            Console.WriteLine("Employee Name = " + emp["Ename"]);
+            Console.WriteLine("Employee Salary = " + emp["salary"]);
+            Console.WriteLine("Employee Works For = " + emp["dePartment"] + " Department");
 
             Console.Read();
         }
---
+++ CSharp/Day5/Day5Csharp/Day5Csharp/IndexersEg2.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+
+namespace Day5Csharp
+{
+    class Users
+    {
+        //declaring an array field
+        private string[] arr = new string[3];
+
+        //define indexer for arr
+        public string this[int i]
+        {
+            get { return arr[i];}
+            set { arr[i] = value; }
+        }
+
+        //2. overloaded indexer for arr
+
+        public string this[string s]
+        {
+            get
+            {
+                foreach(string str in arr)
+                {
+                    if(str.ToLower() == s.ToLower())
+                    {
+                        return str.ToUpper();
+                    }
+                }
+                return null;
+            }
+
+        }
+    }
+    class IndexersEg2
+    {
+        static void Main()
+        {
+            Users user = new Users();
+
+            //setting values using int indexer
+            user[0] = "Nikitha";
+            user[1] = "Nishitha";
+            user[2] = "Naveen T";
+
+            //accessing values using int indexer
+            for(int i=0; i<3;i++)
+            {
+                Console.WriteLine(user[i]);
+            }
+
+            Console.WriteLine("------Accessing values using string indexer---------");
+
+            Console.WriteLine(user["Nishitha"]);
+            Console.WriteLine(user["Naveen"]);
+            Console.WriteLine(user["Nikitha"]);
+            Console.WriteLine("Third Example Execution");
+
+            Indexer3 id3 = new Indexer3();
+            id3[0] = "How";
+            id3[1] = "are";
+            id3[2] = "you";
+            Console.WriteLine(id3[0] + id3[1] + id3[2]);
+
+            id3[0.1f] = "Csharp";
+            id3[1.1f] = "Java";
+            id3[2.1f] = "Python";
+
+            Console.WriteLine(id3[0.1f] + id3[1.1f] + id3[2.1f]);
+            Console.Read();
+        }
+    }
+
+    class Indexer3
+    {
+        private string[] word = new string[3];
+
+        public string this[int x]
+        {
+            get { return word[x]; }
+            set { word[x] = value; }
+        }
+
+        public string this[float f]
+        {
+            get { return word[(int)f]; }
+            set { word[(int)f] = value; }
+        }
+    }
+}
