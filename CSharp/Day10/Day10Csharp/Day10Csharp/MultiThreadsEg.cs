6a2cd70a800f00aa0fc18af7af95c70f6a8d2355
BanurekhaV <rekhasairam@hotmail.com>
2025-07-01 22:54:02 +0530

    Assignment 5

--- CSharp/Day10/Day10Csharp/Day10Csharp/MultiThreadsEg.cs
+++ CSharp/Day10/Day10Csharp/Day10Csharp/MultiThreadsEg.cs
@@ -13,14 +13,15 @@ namespace Day10Csharp
         {
             Console.WriteLine("Main Thread Started...");
 
-            Thread t1 = new Thread(Method1) { Name = "Thread 1" };
-            Thread t2 = new Thread(Method2) { Name = "Thread 2" };
-            Thread t3 = new Thread(Method3) { Name = "Thread 3" };
-            //to strat the threads
-            t1.Start();
-            t2.Start();
-            t3.Start();
-            Console.WriteLine("Main Thread Ending ..");
+            //Thread t1 = new Thread(Method1) { Name = "Thread 1" };
+            //Thread t2 = new Thread(Method2) { Name = "Thread 2" };
+            //Thread t3 = new Thread(Method3) { Name = "Thread 3" };
+            ////to strat the threads
+            //t1.Start();
+            //t2.Start();
+            //t3.Start();
+            //Console.WriteLine("Main Thread Ending ..");
+            ThreadStartFunction();
             Console.Read();
         }
         static void Method1()
@@ -32,7 +33,7 @@ namespace Day10Csharp
         }
 
         static void Method2()
-        {
+        {            
             for (int i = 0; i <= 5; i++)
             {
                 Console.WriteLine("Method 2..: " + i);
@@ -52,6 +53,31 @@ namespace Day10Csharp
             {
                 Console.WriteLine("Method 3..: " + i);
             }
+        }
+
+        //creating threadstart delegate object to start a thread
+        static void ThreadStartFunction()
+        {
+            ThreadStart obj = new ThreadStart(Method3);
+
+            Thread t = new Thread(obj);
+            t.Start();
+
+            ////Multiple ways of instantiating and starting a thread
+            //Thread thread1 = new Thread(new ThreadStart(Method3));
+            //Thread thread2 = new Thread(delegate () { Method3(); });
+            //Thread thread3 = new Thread(() => { Method3(); });
+            //Thread thread4 = new Thread(() => Method3());
+
+            //Anonymous method call using thread delegate
+            Thread thread5 = new Thread(() =>
+            {
+                for(int i=0; i<=5; i++)
+                {
+                    Console.WriteLine("Hi "+ i);
+                }
+            });
+            thread5.Start();
         }
     }
 }
