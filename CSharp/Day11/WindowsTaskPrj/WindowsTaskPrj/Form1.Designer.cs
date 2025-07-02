46cfc47fd68036e534fc5f3cf1dce9610a746b44
BanurekhaV <rekhasairam@hotmail.com>
2025-07-02 15:48:54 +0530

    tasks

---
+++ CSharp/Day11/WindowsTaskPrj/WindowsTaskPrj/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace WindowsTaskPrj
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnClick = new System.Windows.Forms.Button();
+            this.btnTask = new System.Windows.Forms.Button();
+            this.txtMessage2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(117, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Data";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(197, 73);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(292, 20);
+            this.txtMessage.TabIndex = 1;
+            // 
+            // btnClick
+            // 
+            this.btnClick.Location = new System.Drawing.Point(353, 179);
+            this.btnClick.Name = "btnClick";
+            this.btnClick.Size = new System.Drawing.Size(75, 23);
+            this.btnClick.TabIndex = 2;
+            this.btnClick.Text = "Click";
+            this.btnClick.UseVisualStyleBackColor = true;
+            this.btnClick.Click += new System.EventHandler(this.btnClick_Click);
+            // 
+            // btnTask
+            // 
+            this.btnTask.Location = new System.Drawing.Point(524, 179);
+            this.btnTask.Name = "btnTask";
+            this.btnTask.Size = new System.Drawing.Size(75, 23);
+            this.btnTask.TabIndex = 3;
+            this.btnTask.Text = "Click Task";
+            this.btnTask.UseVisualStyleBackColor = true;
+            this.btnTask.Click += new System.EventHandler(this.btnTask_Click);
+            // 
+            // txtMessage2
+            // 
+            this.txtMessage2.Location = new System.Drawing.Point(524, 73);
+            this.txtMessage2.Name = "txtMessage2";
+            this.txtMessage2.Size = new System.Drawing.Size(248, 20);
+            this.txtMessage2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtMessage2);
+            this.Controls.Add(this.btnTask);
+            this.Controls.Add(this.btnClick);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnClick;
+        private System.Windows.Forms.Button btnTask;
+        private System.Windows.Forms.TextBox txtMessage2;
+    }
+}
+
---
+++ CSharp/Day11/WindowsTaskPrj/WindowsTaskPrj/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsTaskPrj
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello and Welcome to Tasks...");
+        }
+
+        private int CountCharacters()
+        {
+            int count = 0;
+            using(StreamReader reader = new StreamReader("Bigfile.txt"))
+            {
+                string content = reader.ReadToEnd();
+                count=content.Length;
+                Thread.Sleep(5000); 
+            }
+            return count;
+        }
+
+        // the below event works synchronously
+        private void btnClick_Click(object sender, EventArgs e)
+        {
+            int z = CountCharacters();
+            txtMessage.Text = "Processing file counting job, Please wait....";
+            txtMessage.Text= z.ToString();
+        }
+
+        private async void btnTask_Click(object sender, EventArgs e)
+        {
+            Task<int> mytask = new Task<int>(CountCharacters);
+            mytask.Start();   // starts to execute the task (method associated with the task)
+            txtMessage.Text = "Processing file counting job, Please wait....";
+            txtMessage2.Text = "This should take some time..";
+            label1.Visible = false;
+            btnClick.Text = "OK";
+            btnClick.ForeColor= System.Drawing.Color.Blue;
+            btnTask.ForeColor = System.Drawing.Color.Red;
+            int z = await mytask;
+            txtMessage.Text = z.ToString() + " " +  "Characters found in the File.";
+        }
+    }
+}
