f5a25d6b7970ba99377be0b125ecd918864504cd
BanurekhaV <rekhasairam@hotmail.com>
2025-07-03 13:39:22 +0530

    queryexpressions

---
+++ CSharp/Day12/Day12Csharp_1/Day12Csharp_1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day12Csharp_1
+{
+    //outer class
+    public class Car
+    {
+         Engine e = new Engine();
+        static string Make = "Ford";
+        public void ShowCar()
+        {
+            Console.WriteLine("Car - Benz");
+            e.ShowEngine();
+        }
+
+        //nested or inner class
+        public class Engine
+        {
+            public void ShowEngine()
+            {
+                Car c = new Car();
+                Console.WriteLine("Diesel Engine" + Car.Make);
+                c.ShowCar();
+                
+            }
+        }
+    }
+
+    //ex 2
+     class Computer
+    {
+        public void Display()
+        {
+            Console.WriteLine("Computer Class Method..");
+        }
+
+        //nested class
+        public class CPU
+        {
+            public void ShowCPU()
+            {
+                Console.WriteLine("It is CPU");
+            }
+        }
+    }
+
+    class Laptop : Computer
+    {
+
+    }
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            Car Suvcar = new Car();
+            Suvcar.ShowCar();
+            
+            Car.Engine Dengine = new Car.Engine();
+            Dengine.ShowEngine();
+
+            Laptop laptop = new Laptop();
+            laptop.Display();
+            Console.Read();
+            
+        }
+    }
+}
---
+++ CSharp/Day12/Day12Csharp_1/Day12Csharp_1/QueryExpressions.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day12Csharp_1
+{
+    class QueryExpressions
+    {
+        static void Main()
+        {
+            int[] intval = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            //Query Expression
+            IEnumerable<int> oddvalues = from v in intval
+                                         where (v % 2) != 0
+                                         select v;
+
+            //iterate the result collection
+            foreach(int x in oddvalues)
+            {
+                Console.WriteLine(x);
+            }
+
+            Console.WriteLine("----Query Expressions with Objects-----");
+            IEnumerable<Student> stdobj = from student in Student.GetStudents()
+                                          where student.Name.EndsWith("n")
+                                          select student;
+
+            foreach(var s in stdobj)
+            {
+                Console.WriteLine($"{s.Id}  {s.Name}  {s.EMail}");
+            }
+
+            //name of the student whose id==103
+           IEnumerable<string> stdname = from s in Student.GetStudents()
+                          where s.Id == 103
+                          select s.Name;
+
+            foreach(var name in stdname)
+            {
+                Console.WriteLine(name);
+            }
+            Console.ReadKey();
+        }
+    }
+
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string EMail { get; set; }
+
+        public static List<Student> GetStudents()
+        {
+            List<Student> stdlist = new List<Student>() 
+            { 
+                new Student{Id = 101, Name="Naveen", EMail="Naveen@gmail.com"},
+                new Student{Id = 102, Name="Nithin", EMail="Nithin@gmail.com"},
+                new Student{Id = 103, Name="Nikitha", EMail="Nikitha@gmail.com"},
+            };
+            //Student s1 = new Student { Id = 101, Name = "Naveen", EMail = "Naveen@gmail.com" };
+            //stdlist.Add(s1);
+            return stdlist;
+        }
+    }
+}
