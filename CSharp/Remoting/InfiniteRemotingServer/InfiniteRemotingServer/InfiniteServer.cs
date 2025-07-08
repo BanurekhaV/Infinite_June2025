001b6d7d07ed8a0626fed7afd1acf11341068e5d
BanurekhaV <rekhasairam@hotmail.com>
2025-07-08 17:11:27 +0530

    remoting

---
+++ CSharp/Remoting/InfiniteRemotingServer/InfiniteRemotingServer/InfiniteServer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace InfiniteRemotingServer
+{
+    public class InfiniteServer : MarshalByRefObject
+    {
+        public string Greetings()
+        {
+            return "Hello Folks !! Welcome to CSharp Remoting...";
+        }
+    }
+
+    //service class
+    public class InfiniteServices : MarshalByRefObject
+    {
+        public int WriteData(int n1, int n2)
+        {
+            int maxval = (Math.Max(n1, n2));
+            Console.WriteLine("Call Executed...");
+            return maxval;
+        }
+    }
+}
