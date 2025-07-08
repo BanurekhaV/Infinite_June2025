93cc58170354345564d620f43f8e4ac9715b3704
BanurekhaV <rekhasairam@hotmail.com>
2025-07-08 11:35:36 +0530

    pattern matching

---
+++ CSharp/Day14/Day14Csharp/Day14Csharp/PatternMatching2.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14Csharp
+{
+    public class Shape
+    {
+        public const float PI = 3.14f;
+    }
+
+    public class Circle : Shape
+    {
+        public double Radius { get; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+    }
+
+    public class Rectangle: Shape
+    {
+        public double Length { get; }
+        public double Breadth { get; }
+
+        public Rectangle(double l, double b)
+        {
+            Length = l;
+            Breadth = b;
+        }
+    }
+
+    public class Triangle : Shape
+    {
+        public double Base { get; }
+        public double Height { get; }
+        
+        public Triangle(double @base, double height)
+        {
+            Base = @base;
+            Height = height;
+        }
+
+    }
+    class PatternMatching2
+    {
+        //prior to c# 7.0 . Testing each type in a series and then casting it accordingly
+        //public static void DisplayArea(Shape shape)
+        //{
+        //    if(shape is Circle)
+        //    {
+        //        Circle c = (Circle)shape;
+        //        Console.WriteLine("Area of Circle is : " + c.Radius * c.Radius * Shape.PI);
+        //    }
+        //    else if (shape is Rectangle)
+        //    {
+        //       Rectangle r = (Rectangle)shape;
+        //        Console.WriteLine("Area of Rectangle is : " + r.Length * r.Breadth);
+        //    }
+        //    else if (shape is Triangle)
+        //    {
+        //        Triangle t = (Triangle)shape;
+        //        Console.WriteLine("Area of Triangle is : " + 0.5 * t.Base * t.Height);
+        //    }
+        //    else
+        //    {
+        //        throw new ArgumentException(message: "Invalid Shape", paramName: nameof(shape));
+        //    }
+        //}
+
+        //with C# 7.0
+        //public static void DisplayArea(Shape shape)
+        //{
+        //    if (shape is Circle c)
+        //    {
+        //       Console.WriteLine("Area of Circle is : " + c.Radius * c.Radius * Shape.PI);
+        //    }
+        //    else if (shape is Rectangle r)
+        //    {
+        //       Console.WriteLine("Area of Rectangle is : " + r.Length * r.Breadth);
+        //    }
+        //    else if (shape is Triangle t)
+        //    {
+        //       Console.WriteLine("Area of Triangle is : " + 0.5 * t.Base * t.Height);
+        //    }
+        //    else
+        //    {
+        //        throw new ArgumentException(message: "Invalid Shape", paramName: nameof(shape));
+        //    }
+        //}
+
+        //with switch and when
+
+        public static void DisplayArea(Shape shape)
+        { 
+           switch(shape)
+            {
+                case Rectangle r when r.Length==r.Breadth:
+                    Console.WriteLine("Area of Square is : " + r.Length * r.Breadth);
+                    break;
+                case Rectangle r:
+                    Console.WriteLine("Area of Rectangle is : " + r.Length * r.Breadth);
+                    break;
+                case Circle c:
+                    Console.WriteLine("Area of Circle is : " + c.Radius * c.Radius * Shape.PI);
+                    break;               
+                case Triangle t:
+                    Console.WriteLine("Area of Triangle is : " + 0.5 * t.Base * t.Height);
+                    break;                
+                case null:
+                    throw new ArgumentNullException(nameof(shape));
+                default:
+                    throw new ArgumentException(message: "Invalid Shape", paramName: nameof(shape));
+            }
+        }
+        static void Main()
+        {
+            Rectangle square = new Rectangle(5, 5);
+            DisplayArea(square);
+            Rectangle rectangle = new Rectangle(6, 7);
+            DisplayArea(rectangle);
+            Circle circle = new Circle(4);
+            DisplayArea(circle);
+            Triangle triangle = new Triangle(6, 5);
+            DisplayArea(triangle);
+            Console.Read();
+        }
+    }
+}
---
+++ CSharp/Day14/Day14Csharp/Day14Csharp/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14Csharp
+{
+    class Person
+    {
+        public string Name { get; set; }
+    }
+    class Program
+    {
+        static void checkType(object obj)
+        {
+            switch(obj)
+            {
+                case Person p:
+                    Console.WriteLine("Obj is Person Type");
+                     Console.WriteLine($"Nameof of the Person : { p.Name}");
+                    break;
+                case int i:
+                    Console.WriteLine("Obj is Integer");
+                    Console.WriteLine($"Value of int : {i}");
+                    break;
+                default:
+                    Console.WriteLine("Obj is some other type");
+                    break;
+            }
+        }
+
+        //public static string GetNumberSign(int n)
+        //{
+            //switch(n)
+            //{
+            //    case < 0:
+            //        return "Negative Number";
+            //    case 0:
+            //        return "Number Zero";
+            //    case >= 1:
+            //        return "Positive Number";
+            //}
+            //return n switch
+            //{
+            //    < 0 => "Negative Number",
+            //    0 => "Zero",
+            //    >= 1 => "Positive Number"
+            //};
+      //  }
+
+        //property pattern
+        //public static void WriteStringLenth(string str)
+        //{
+        //    if(str is null)
+        //    {
+        //        Console.WriteLine("It is a Null String");
+        //    }
+        //    if (str is { Length: 0 })
+        //    {
+        //        Console.WriteLine("empty string");
+        //        return;
+        //    }
+        //    if (str is { Length: 1 })
+        //    {
+        //        Console.WriteLine("it is a string of length 1");
+        //        return;
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("String of more than 1 length");
+        //        return;
+        //    }
+        //}
+
+        //positional pattern
+        public struct CheckBooleanData
+        {
+            public bool Data1 { get; set; }
+            public bool Data2 { get; set; }
+
+            public void unWrap(out bool d1, out bool d2)
+            {
+                d1 = Data1;
+                d2 = Data2;
+            }
+        }
+
+        //positional pattern with logical and and or
+        //public static bool LogicalAnd(CheckBooleanData n1)
+        //{
+        //    //using switch expressions
+        //    return n1 switch
+        //    {
+        //        (false, false) => false,
+        //        (true, false) => false,
+        //        (false, true) => false,
+        //        (true, true) => true
+        //    };
+        //}
+
+        //public static bool LogicalOR(CheckBooleanData n1)
+        //{
+        //    //using switch expressions
+        //     switch(n1)
+        //     {
+        //        case (false, false) return false,
+        //        case (true, false) return  true,
+        //        (false, true) return true,
+        //        (true, true) return true
+        //    };
+        //}
+        static void Main(string[] args)
+        {
+            var person = new Person { Name = "Mehrun" };
+            checkType(56);
+            checkType(person);
+            checkType("Hi");
+
+            //property pattern driver
+            //WriteStringLenth("CSharp");
+            //WriteStringLenth("");
+            //WriteStringLenth("A");
+
+                      
+            Console.Read();
+        }
+    }
+}
