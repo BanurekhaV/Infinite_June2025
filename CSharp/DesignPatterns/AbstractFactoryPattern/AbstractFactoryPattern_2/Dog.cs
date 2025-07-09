fad05ad97d3063f3aef443d9883d978bf8fa0e9e
BanurekhaV <rekhasairam@hotmail.com>
2025-07-09 18:40:52 +0530

    design patterns

---
+++ CSharp/DesignPatterns/AbstractFactoryPattern/AbstractFactoryPattern_2/Dog.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactoryPattern_2
+{
+    public class Dog : IAnimal
+    {
+        public string Speak()
+        {
+            return "Bow Bow";
+        }
+    }
+
+    public class Cat : IAnimal
+    {
+        public string Speak()
+        {
+            return "Meow Meow";
+        }
+    }
+
+    public class Shark : IAnimal
+    {
+        public string Speak()
+        {
+            return "Cannot Speak";
+        }
+    }
+
+    public class Octopus : IAnimal
+    {
+        public string Speak()
+        {
+            return " Squawck";
+        }
+    }
+}
---
+++ CSharp/DesignPatterns/AbstractFactoryPattern/AbstractFactoryPattern_2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactoryPattern_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IAnimal animal = null;
+            AnimalFactory animalfactory = null;
+            string sound = null;
+
+            //create factory objects
+            Console.WriteLine("Enter the type of Animal (Land/Sea) ");
+            string atype = Console.ReadLine();
+            animalfactory = AnimalFactory.CreateAnimalFactory(atype);
+            Console.WriteLine("Animal Type Chosen : " + animalfactory.GetType().Name);
+
+            //create animal objects
+            Console.WriteLine("Enter The Actual Animal :");
+            string animalname = Console.ReadLine();
+            animal = animalfactory.GetAnimal(animalname);
+            Console.WriteLine($"Animal Chosen is {animalname}");
+
+            sound = animal.Speak();
+            Console.WriteLine($"The sound of the {animal} coming from the Factory {animalfactory}" +
+                $" is {sound}");
+            Console.Read();
+        }
+    }
+}
