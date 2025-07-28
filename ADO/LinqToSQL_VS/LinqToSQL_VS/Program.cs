f391e0a79ef29870a9ba8254baeefe1bc7550278
BanurekhaV <rekhasairam@hotmail.com>
2025-07-28 18:31:55 +0530

    Linq to Sql

---
+++ ADO/LinqToSQL_VS/LinqToSQL_VS/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqToSQL_VS
+{
+    class Program
+    {
+        static LS_VSDataContext db = new LS_VSDataContext();  //inoke the empty constructor
+        static void Main(string[] args)
+        {
+            var emp = db.emps.ToList();
+
+            foreach(var e in emp)
+            {
+                Console.WriteLine($"{e.empno} {e.ename} {e.job} {e.salary} {e.mgr_id}");
+            }
+            Console.Read();
+        }
+    }
+}
