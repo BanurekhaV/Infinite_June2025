7caf0de4a2fc4299aba0930dd677366cc72c8a89
BanurekhaV <rekhasairam@hotmail.com>
2025-08-14 11:37:15 +0530

    asp exceptions

---
+++ ASP/Exception_Prj/Exception_Prj/ErrorsPage.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ErrorsPage.aspx.cs" Inherits="Exception_Prj.ErrorsPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Application Error</h1>
+            <p>An Unknown Exception Occurred..</p>
+            <p>For further assistance contact helpdesk@support.infinite.com</p>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ASP/Exception_Prj/Exception_Prj/XMLForm.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace Exception_Prj
+{
+    public partial class XMLForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //try
+            //{
+            DataSet ds = new DataSet();
+                //Response.Redirect("Hello.aspx");
+                ds.ReadXml(Server.MapPath("~/Employees.xml"));
+                Grid1.DataSource = ds;
+                Grid1.DataBind();
+            //}
+            //catch(Exception ex)
+            //{
+            //    Response.Write(ex.Message);
+            //    lblmsg.Text = "Some Technical Error Occurred.. try after sometime";
+            //}
+        }
+
+        protected void Page_Error(Object sender, EventArgs e)
+        {
+            Exception ex = Server.GetLastError();
+            Server.ClearError();
+            Response.Write(ex.GetType());
+            Response.Redirect("~/ErrorsPage.aspx");
+
+        }
+
+    }
+}
