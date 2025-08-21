c1be6a84c6180a6c388ec956890e876c489b5222
BanurekhaV <rekhasairam@hotmail.com>
2025-08-21 11:19:14 +0530

    MVC Day4

---
+++ MVC/HTML_HelpersPrj/HTML_HelpersPrj/CustomHelpers/CustomExtensionHelpers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HTML_HelpersPrj.CustomHelpers
+{
+    public static class CustomExtensionHelpers
+    {
+        public static IHtmlString RedLabel(this HtmlHelper helperobj,string content)
+        {
+            string str = String.Format("<label><i><font color=red>{0}</font></i></label>", content);
+            return new HtmlString(str);
+        }
+
+        public static IHtmlString GreenLabel(this HtmlHelper helperobj, string content)
+        {
+            string str = String.Format("<label><i><font color=green>{0}</font></i></label>", content);
+            return new HtmlString(str);
+        }
+    }
+}
---
+++ MVC/ModelStates_Prj/ModelStates_Prj/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace ModelStates_Prj.Models
+{
+    public class User
+    {
+        [Required(ErrorMessage ="FirstName Required")]
+        [Display(Name ="First Name")]
+        [StringLength(20,ErrorMessage ="First Name Cannot be more than 20 characters")]
+        public string Fname { get; set; }
+
+        [DisplayName("Last Name")]
+        public string Lname { get; set; }
+
+        [Display(Name ="Users Age")]
+        [Range(21,45,ErrorMessage ="Age has to be between 21 and 45 only")]
+        public int age { get; set; }
+
+        [Required(ErrorMessage ="Please enter your Email")]
+        [Display(Name ="Email")]
+        [RegularExpression(@"^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})$",
+            ErrorMessage ="Invalid Format")]
+        public string email { get; set; }
+    }
+}
