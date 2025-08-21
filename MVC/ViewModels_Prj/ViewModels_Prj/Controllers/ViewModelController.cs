815a4efac4dc7ce7a0f18b257c1146fa2ec3ded0
BanurekhaV <rekhasairam@hotmail.com>
2025-08-21 13:38:00 +0530

    MVC Day4

---
+++ MVC/ViewModels_Prj/ViewModels_Prj/Controllers/ViewModelController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ViewModels_Prj.Models;
+
+namespace ViewModels_Prj.Controllers
+{
+    public class ViewModelController : Controller
+    {
+        // GET: ViewModel
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult EmpAddDetails()
+        {
+            Employee e = new Employee()
+            {
+                EID = 101,
+                EName = "Harshitha",
+                Salary = 55000,
+                AddreesId = 1
+            };
+            Address addr = new Address()
+            {
+                AddressId = 1,
+                DoorNo = "4, ABC Villa",
+                Street = "GulliNo 420",
+                City = "MyCity"
+            };
+
+            //create a viewmodel object
+            EmployeeAddress empadd = new EmployeeAddress()
+            {
+                employee = e,
+                address = addr,
+                PageTitle = "Employee Personal Details"
+            };
+            //ViewBag.VmTitle = "View Model Example";
+            return View(empadd);
+        }
+    }
+}
---
+++ MVC/ViewModels_Prj/ViewModels_Prj/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ViewModels_Prj.Models
+{
+    public class Employee
+    {
+        public int EID { get; set; }
+        public string EName { get; set; }
+        public decimal Salary { get; set; }
+        public int ? AddreesId { get; set; }
+    }
+}
