4fddd1a890a0d3077fc406806488c10d928f1d35
BanurekhaV <rekhasairam@hotmail.com>
2025-08-28 18:41:30 +0530

    mvc day8

---
+++ Web API/WebApi_1/WebApi_1/Controllers/DemoController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApi_1.Controllers
+{
+    public class DemoController : ApiController
+    {
+        static List<string> continents = new List<string>()
+        {
+            "Asia",
+            "Africa",
+            "America",
+            "Antartica",
+            "Australia",
+            "Europe"
+        };
+
+        //Get : api/Demo
+        public IEnumerable<string> Get()
+        {
+            return continents;
+        }
+
+        public string Get(int Id)
+        {
+            return continents[Id - 1];
+        }
+
+        public IEnumerable<string>Post([FromBody] string c)
+        {
+            continents.Add(c);
+            return continents;
+        }
+
+        public IEnumerable<string>Put(int Id, [FromUri] string c)
+        {
+            continents[Id - 1] = c;
+            return continents;
+        }
+
+        public IEnumerable<string>Delete(int Id)
+        {
+            continents.RemoveAt(Id - 1);
+            return continents;
+        }
+    }
+}
---
+++ Web API/WebApi_1/WebApi_1/Controllers/PersonController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi_1.Models;
+
+namespace WebApi_1.Controllers
+{
+    [RoutePrefix("api/User")]
+    public class PersonController : ApiController
+    {
+        List<Person> personlist = new List<Person>()
+        {
+            new Person{Id=1, Personname= "Yudhishter", PersonJob= "King", Gender="Male"},
+            new Person{Id=2, Personname= "Draupadi", PersonJob= "Queen", Gender="Female"},
+            new Person{Id=3, Personname= "Bheem", PersonJob= "Defence Minister", Gender="Male"},
+            new Person{Id=4, Personname= "Arjun", PersonJob= "Archerer", Gender="Male"},
+            new Person{Id=5, Personname= "Nakul", PersonJob= "Operations", Gender="Male"},
+        };
+
+        [HttpGet]
+        [Route("All")]
+        public IEnumerable<Person> Get()
+        {
+            return personlist;
+        }
+
+        [HttpGet]
+        [Route("Bymsg")]
+        public HttpResponseMessage GetAllPersons()
+        {
+            //creating a response  object with both the data and the status code
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, personlist);
+
+            ////incase we donot want to send data but only response
+            //HttpResponseMessage response = response.Content = new StringContent("Thanks");
+            return response;
+        }
+    }
+}
