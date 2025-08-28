c19ad4967a138fb5fa84282032a39fc4df033539
BanurekhaV <rekhasairam@hotmail.com>
2025-08-28 11:32:56 +0530

    mvc day8

---
+++ MVC/Security_Prj/Security_Prj/Controllers/AccountsController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using Security_Prj.Models;
+
+namespace Security_Prj.Controllers
+{
+    public class AccountsController : Controller
+    {
+        // GET: Accounts
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(User model)
+        {
+            using(MVCSecurityDBEntities context = new MVCSecurityDBEntities())
+            {
+                bool IsValidUser = context.Users.Any(user => user.UserName.ToLower() ==
+                model.UserName.ToLower() && user.UserPassword == model.UserPassword);
+
+                if(IsValidUser)
+                {
+                    FormsAuthentication.SetAuthCookie(model.UserName, false);
+                    return RedirectToAction("Index", "Employees");
+                }
+                ModelState.AddModelError("", "Invalid Username or Password");
+                return View();
+            }
+          
+        }
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(User model)
+        {
+            using (MVCSecurityDBEntities context = new MVCSecurityDBEntities())
+            {
+                context.Users.Add(model);
+                context.SaveChanges();
+            }
+            return RedirectToAction("Login");
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
